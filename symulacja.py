e3fdff52f78cd382b637d8f23969545b2db97b9b
Afraiven <filip.antoniak99@gmail.com>
2024-06-05 18:19:03 +0200

    Poprawiony algorytm i prototyp wizualizacji

--- symulacja.py
+++ symulacja.py
@@ -1,52 +1,153 @@
+import time
+import pygame
+import random
+from itertools import groupby
+from wizualizacja import draw_elevator
+
+debug = False
 poziom = 0
 trasa = []
 requesty = []
+osoby_w_windzie = []
+osoby_na_piętrach = [[] for _ in range(11)]  # Przykład dla budynku z 11 piętrami (0-10)
 
-# generuje trase przejazdu windy
+def flatten(lst):
+    flat_list = []
+    for item in lst:
+        if isinstance(item, list):
+            flat_list.extend(flatten(item))
+        else:
+            flat_list.append(item)
+    return flat_list
+
+def remove_consecutive_duplicates(data):
+    return [key for key, _ in groupby(data)]
+
 def generuj_trase(trasa, requesty, poziom):
     if len(requesty) == 0:
         return trasa, requesty
     
     # dodaj pierwszy request do trasy
-    trasa.append(requesty[0][0])
-    trasa.append(requesty[0][1])
-    requesty.remove(requesty[0])
+    if len(trasa) == 0:
+        trasa.append(requesty[0][0])
+        trasa.append(requesty[0][1])
+        requesty.remove(requesty[0])
 
-    # okresl kierunek jazdy windy
-    if requesty[0][0] < requesty[0][1]:
-        kierunek = 'up'
-        potencjalne = [x for x in requesty if x[0] < x[1]]
-        for j in potencjalne:
-            trasa.append(j[0])
-            trasa.append(j[1])
-            requesty.remove(j)
-        trasa = sorted(trasa)
+        if len(requesty) == 0:
+            return trasa, requesty
+        # okresl kierunek jazdy windy
+        if requesty[0][0] < requesty[0][1]:
+            kierunek = 'up'
+            potencjalne = [x for x in requesty if x[0] < x[1]]
+            for j in potencjalne:
+                trasa.append(j[0])
+                trasa.append(j[1])
+                requesty.remove(j)
+            trasa = sorted(trasa)
+        else:
+            kierunek = 'down'
+            potencjalne = [x for x in requesty if x[0] > x[1]]
+            for j in potencjalne:
+                trasa.append(j[0])
+                trasa.append(j[1])
+                requesty.remove(j)
+            trasa = sorted(trasa)[::-1]
+   
     else:
-        kierunek = 'down'
-        potencjalne = [x for x in requesty if x[0] > x[1]]
-        for j in potencjalne:
-            trasa.append(j[0])
-            trasa.append(j[1])
-            requesty.remove(j)
-        trasa = sorted(trasa)[::-1]
-    print(trasa)
-    return trasa, requesty
+        trasa = trasa[::-1]
+        trasa.append(poziom)
+        trasa = trasa[::-1]
+        for request in requesty:
+            if request[0] < request[1]:
+                kierunek = 'up'
+                for i in range(len(trasa)-1):
+                    if type(trasa[i]) == list:
+                        now = max(trasa[i])
+                    else:
+                        now = trasa[i]
+                    if type(trasa[i+1]) == list:
+                        next = max(trasa[i+1])
+                    else:
+                        next = trasa[i+1]
+                    if now < next:
+                        if now <= request[0] <= next:
+                            trasa.insert(i, request)
+                            requesty.remove(request)
+                            break
+            else:
+                kierunek = 'down'
+                for i in range(len(trasa)-1):
+                    if type(trasa[i]) == list:
+                        now = max(trasa[i])
+                    else:
+                        now = trasa[i]
+                    if type(trasa[i+1]) == list:
+                        next = max(trasa[i+1])
+                    else:
+                        next = trasa[i+1]
+                    if now > next:
+                        if now >= request[0] >= next:
+                            trasa.insert(i+1, request)
+                            requesty.remove(request)
+                            break
+        #[0, 10, 7, 6, 5, 0]
+        #[[3, 6]]
+    nowa_trasa = []
+    for i in trasa:
+        if i == poziom:
+            trasa.remove(i)
+            break
+    kopia_trasy = trasa.copy()
+    for i in trasa:
+        if i in kopia_trasy:
+            kopia_trasy.remove(i)
+            nowa_trasa.append(i)
+    nowa_trasa = flatten(nowa_trasa)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+    return nowa_trasa, requesty
 
-for i in range(30):
-    # losowanie requestów [tymczasowe statyczne]
-    if i == 0:
-        requesty.append([8, 1])
-        requesty.append([7, 0])
-    # losowanie requestów [tymczasowe statyczne]
-    elif i == 35:
-        requesty.append([3, 6])
-    trasa, requesty = generuj_trase(trasa, requesty, poziom)
+while True:
+    for _ in range(random.randint(1, 1)):
+        prob = random.randint(0, 9)
+        if prob == 0:
+            start = random.randint(0, 10)
+            stop = random.randint(0, 10)
+            while start == stop:
+                stop = random.randint(0, 10)
+            requesty.append([start, stop])
+            print(f" Chce jechać z {start} na {stop}")
+            osoby_na_piętrach[start].append(stop)  # Dodaj osoby oczekujące na piętrach
 
-    # symulacja jazdy windy
-    if len(trasa) > 0:
-        poziom += [-1, 1][trasa[0] > poziom] 
+    if len(requesty) > 0:
+        trasa = remove_consecutive_duplicates(trasa)
+        trasa, requesty = generuj_trase(trasa, requesty, poziom)
+
+    if debug:
+        print(f"Piętro: {poziom}")
+        print(f"Trasa: {trasa}")
+        print(f"Requesty: {requesty}")
+        print(f"Osoby na piętrach: {osoby_na_piętrach}")
+        print(f"Osoby w windzie: {osoby_w_windzie}")
+
+    if len(trasa) == 0:
+        draw_elevator(poziom, poziom, osoby_na_piętrach, osoby_w_windzie)
+    else:
+        draw_elevator(poziom, trasa[0], osoby_na_piętrach, osoby_w_windzie)
         if poziom == trasa[0]:
             trasa.remove(trasa[0])
-            print('Zatrzymano')
-    print(poziom)
-    input()
+            if debug:
+                print('Zatrzymano')
+            if len(osoby_na_piętrach[poziom]) > 0:
+                # DODAC WARUNEK JEZELI ZABIERA
+                for x in osoby_na_piętrach[poziom]:
+                    osoby_w_windzie.append(x)
+                osoby_na_piętrach[poziom] = []
+            for x in osoby_w_windzie:
+                if x == poziom:
+                    osoby_w_windzie.remove(x)
+            time.sleep(0.5)
+        if len(trasa) != 0:
+            poziom += [-1, 1][trasa[0] > poziom] * (poziom != trasa[0])
+pygame.quit()
--- wizualizacja.py
+++ wizualizacja.py
@@ -1,13 +1,10 @@
 import time
 import pygame
-import random 
+import os
 
-random.seed(1234)
-levels = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-timer = True
-time_step = 0.5
-current = 0
-debug = False
+
+time_step = 0.3
+os.environ['SDL_VIDEO_WINDOW_POS'] = '100,100'
 
 # Pygame setup
 pygame.init()
@@ -22,150 +19,32 @@ def draw_multiline_text(surface, text, pos, font, color):
     for line in text.split('\n'):
         line_surface = font.render(line, True, color)
         surface.blit(line_surface, (x, y))
-        y += line_spacing 
+        y += line_spacing
 
-def draw_elevator(position, desired):
+def draw_elevator(position, desired, osoby_na_piętrach, osoby_w_windzie):
     screen.fill((0, 0, 0))
     floor_height = 50
     elevator_y = 600 - (position * floor_height) - floor_height
-    pygame.draw.rect(screen, (255, 255, 255), (100, elevator_y, 100, 100))
-    multi_line_text = f"Piętro: {position}\nCel: {desired}"
-    text_x = 100  
-    text_y = elevator_y + 5  
+    pygame.draw.rect(screen, (255, 255, 255), (100, elevator_y, 100, 50))
+    multi_line_text = f"Piętro: {position}\nCel: {desired}\nIlosc: {osoby_w_windzie}"
+    text_x = 100
+    text_y = elevator_y + 5
     draw_multiline_text(screen, multi_line_text, (text_x, text_y), font, (0, 128, 0))
+
+    # Draw levels
+    for level in range(11):  # Levels 0 to 10
+        level_y = 600 - (level * floor_height) - floor_height
+        pygame.draw.rect(screen, (200, 200, 200), (210, level_y + 50, 80, 2))  # Draw level line
+        level_text = f"{level}"
+        draw_multiline_text(screen, level_text, (220, level_y + 25), font, (255, 255, 255))
+
+        occupied_levels = [x for x in range(len(osoby_na_piętrach)) if len(osoby_na_piętrach[x]) > 0]
+   
+        # Check if the level is in the list to draw a person
+        if level in occupied_levels:
+            # Draw a simple person (circle for the head and rectangle for the body)
+            pygame.draw.circle(screen, (255, 0, 0), (260, level_y + 30), 10)  # Head
+            pygame.draw.rect(screen, (255, 0, 0), (255, level_y + 40, 10, 20))  # Body
+
     pygame.display.flip()
     time.sleep(time_step)
-
-
-
-def go_up(start, levels, desired):
-    global current 
-    if debug:
-        print(f"Jazda w góre, Winda jest na piętrze {current}, pasażer zaczyna jazdę z piętra {start}, a cel to {desired}")
-        print("*"*20)
-    # jeżeli winda jest za nisko to jedzie do góry
-    if start > current:
-        current = go_up(current, levels, start)
-    elif start < current:
-        current = go_down(current, levels, start)
-    # funkcja opisująca ruch windy w górę
-   
-    for i in range(start, desired + 1):
-        if debug:
-            print('Winda jest na piętrze', levels[i])
-        draw_elevator(levels[i], desired)
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                
-    if debug:
-        print("*"*20)
-    time.sleep(3)
-    return desired
-
-def go_down(start, levels, desired=0):
-    global current
-    if debug:
-        print(f"Jazda w dół, Winda jest na piętrze {current}, pasażer zaczyna jazdę z piętra {start}, a cel to {desired}")
-        print("*"*20)
-    # jeżeli winda jest za nisko to jedzie do góry
-    if start > current:
-        current = go_up(current, levels, start)
-    elif start < current:
-        current = go_down(current, levels, start)
-    # funkcja opisująca ruch windy w dół
-    
-    for i in range(start, desired - 1, -1):
-        if debug:
-            print('Winda jest na piętrze', levels[i])
-        draw_elevator(levels[i], desired)
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-    if debug:
-        print("*"*20)
-    time.sleep(3)
-    return desired
-
-for i in range(10):
-    start = random.choice(levels)
-    desired = random.choice(levels)
-    while desired == start:
-        desired = random.choice(levels)
-    print(f"Start: {start}Cel: {desired}")
-    if start < desired:
-        current = go_up(start, levels, desired)
-    elif start > desired:
-        current = go_down(start, levels, desired)
-    time.sleep(1)
-
-# current = 0
-# current = go_up(current, 0, levels, 5)
-# current = go_down(current, 10, levels)
-pygame.quit()
-
-# TODO
-# Asynchroniczny ruch windy względem requestów od pasażerów
-# Requesty w ustalonym typie danych ( stos / kolejka / lista)
-# Wielu pasażerów jednocześnie
-# Limit pasażerów
-
-
-# import threading
-# import time
-
-
-# class Pasazer():
-#     def __init__(self, start) -> None:
-#         self.start = start
-
-#     # planuje jechać w dół z pietra start
-#     def call_down(self):
-#         # jeśli jedzie i ma po drodze to zatrzymaj
-#         # jesli jedzie i nie ma po drodze to dodaj na koniec trasy
-#         # jesli stoi to dodaj do trasy
-#         global poziom
-#         global trasa
-
-#     # planuje jechać w górę z pietra start
-#     def call_up(self):
-#         # jeśli jedzie i ma po drodze to zatrzymaj
-#         # jesli jedzie i nie ma po drodze to dodaj na koniec trasy
-#         # jesli stoi to dodaj do trasy
-#         global poziom
-#         global trasa
-
-#     # pasażer w windzie wybiera cel
-#     def jedz_na(self, cel):
-#         global requesty
-#         requesty.append([self.start, cel])
-
-
-# class Winda():
-#     def __init__(self):
-#         self.poziom = 0
-#         self.trasa = []
-#         self.requesty = []
-#         self.stop = False
-#         self.going_up = False
-
-#     def generuj_trasa(self):
-#         # generuje trasę na podstawie requestów
-        
-
-#     def jedz(self):
-#         while len(trasa) > 0:
-#             self.fix_trasa()
-            
-
-# poziom = 0
-# trasa = []
-# winda = Winda()
-# filip = Pasazer(8)
-# filip.call_down()
-# filip.jedz_na(0)
-# kuba = Pasazer(7)
-# kuba.call_down()
-# kuba.jedz_na(0)
-# winda.jedz()
-
